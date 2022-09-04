3f4e2c70ee5c2dbcf11fec1026d40b4e91a35637
DTimothyR <112803809+DTimothyR@users.noreply.github.com>
2022-09-04 16:16:32 +0200

    Update CAPShiny.lua

--- CAPShiny.lua
+++ CAPShiny.lua
@@ -7,7 +7,7 @@ local Chat = game:GetService("Players").LocalPlayer.PlayerGui.Chat.Frame.ChatCha
 
 
 Chat.ChildAdded:Connect(function(instance)
-    if string.find(instance.TextLabel.Text,_G.Username) then
+    if string.find(instance.TextLabel.Text,"Timothy") then
         local OSTime = os.time()
         local Webhook = _G.Webhook
         local Time = os.date('!*t', OSTime)
